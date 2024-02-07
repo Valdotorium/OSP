1f5419ef03b6cbf58792d2c1c2e08d2999dda170
Valdotorium <116170877+Valdotorium@users.noreply.github.com>
2024-02-07 21:47:46 +0100

    basics

---
+++ src/canvas.js
@@ -0,0 +1,6 @@
+let context = document.getElementById("canvas")
+let canvas = context.getContext("2d")
+canvas.moveTo(0, 0)
+canvas.lineTo(100, 100)
+canvas.stroke()
+
---
+++ src/py/main.py
@@ -0,0 +1,8 @@
+
+import arrr
+from pyscript import document
+
+
+txt = document.querySelector("python")
+txt.innerText = "gvgvgzv"
+
